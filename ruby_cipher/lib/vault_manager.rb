5fe1082f66d452eb1af7c682ed03dc6295cf0a08
Mahesh R Yewate <maheshyewate212@gmail.com>
2026-08-19 13:32:18 +0530

    Optimize methods in VaultManager class
    Refactor methods to use appropriate Enumerable methods.

--- ruby_cipher/lib/vault_manager.rb
+++ ruby_cipher/lib/vault_manager.rb
@@ -30,25 +30,20 @@ class VaultManager
 
 
   def all_titles
-    # Use @entries.map here
     @entries.map(&:title)
   end
 
-  # 2. Filter entries by category
+  
   def filter_by_category(target_category)
-    # Use @entries.select here
     @entries.select {|entry| entry.category.downcase == target_category.downcase}
   end
 
-  # 3. Categorize into a Hash
   def entries_by_category
-    # Use @entries.group_by here
     @entries.group_by do |entry|
       entry.category
     end
   end
 
-  # 4. Sum up total words in all entries
   def total_word_count
     @entries.reduce(0) do |sum, entry|
     sum + entry.content.split.size
